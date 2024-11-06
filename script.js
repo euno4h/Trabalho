bf2bbae299ff90110f110eb81f8ce1249b91f068
euno4h <163136318+euno4h@users.noreply.github.com>
2024-11-06 08:37:33 -0300

    .

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ function AdicionarParagrafo(){
 //Remove o ultimo parágrafo, se houver mai um
 function removerParagrafo(){
      const paragrafos = document.querySelectorAll("p");
-     if 
+     if (paragrafos.length > 1) {
+        paragrafos[paragrafos.length - 1].remove();
+     }else{
+        alert("Não há mais parágrafos para remover 🥺 !!");
+     }
+}
+
+//Muda a cor de fundo da página para uma cor aleatória
+function mudarCorFundo(){
+    const corAleatoria = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = corAleatoria;
+}
+
+//Mudar a cor do texto do parágrafo inicial para uma cor aleatória
+function mudarCorTexto(){
+    const cores = ["#FF5733", "#33FF57", "#3357FF", "#FF33A6", "#FFC133"];
+    document.getElementById("text").style.color = cores[Math.floor(Math.random() * cores.length)];
 }
